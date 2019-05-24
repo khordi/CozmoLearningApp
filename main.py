c890a526936c0c21b6a5abd70ae695b3d459bac1
khordi <44495894+khordi@users.noreply.github.com>
2019-05-24 01:20:27 +0100

    Added basic algebra layout

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import cozmo
+
+from sympy.interactive import printing
+printing.init_printing(use_latex=True)
+from sympy import Eq, solve_linear_system, Matrix
+import sympy as sp
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -6,34 +12,52 @@ from kivy.config import Config
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
-
+#-----------------------------------------------------------------
 def cozmo_program(self: cozmo.robot.Robot):
     self.say_text("Hello and welcome to Cozmo Learning", play_excited_animation=True,voice_pitch=-1).wait_for_completed()
 
 def cozmo_button(self: cozmo.robot.Robot):
     self.say_text("hi").wait
 
+
+    
+#------------First Screen-------------------------------------------
 class MainWindow(Screen):
     def on_pre_enter(self):
         Window.size = (608,600)
     def btn(self):
         show_popup()
-        cozmo.run_program(cozmo_button)
+        #cozmo.run_program(cozmo_button)
     
-    cozmo.run_program(cozmo_program)
+    #cozmo.run_program(cozmo_program)
 
 
 class AlegbraWindow(Screen):
     def on_pre_enter(self):
-        Window.size = (900,900)
+        Window.size = (608,600)
+    def algebraSolver(self):
+        #x = 0
+        #y = 0
+        #eq1 = sp.Function('eq1')
+        #eq2 = sp.Function('eq2')
+
+        #eq1 = Eq(2*x - y,-4)
+        #eq2 = Eq(3*x - y,-2)
+        
+        x,y = sp.symbols('x y')
+        row1 = [2, -2, -4]
+        row2 = [6, -8, -2]
+
+        system = Matrix((row1,row2))
+        print(solve_linear_system(system,x,y))
 
 class TrigPythWindow(Screen):
     def on_pre_enter(self):
-        Window.size = (900,900)
+        Window.size = (608,600)
 
 class GeometryWindow(Screen):
     def on_pre_enter(self):
-        Window.size = (900,900)
+        Window.size = (608,600)
 
 
 class WindowManager(ScreenManager):
@@ -48,11 +72,11 @@ class MyMainApp(App):
         self.title = 'Cozmo Learning App'
         return kv
 
-class P(FloatLayout):
+class HELP(FloatLayout):
     pass
 
 def show_popup():
-    show = P() # Create a new instance of the P class 
+    show = HELP() # Create a new instance of the P class 
 
     popupWindow = Popup(title="Help", content=show, size_hint=(None,None),size=(400,400)) 
     # Create the popup window
