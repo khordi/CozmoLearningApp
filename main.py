361dddefd6f2fbd3652b5a81f582bccf63b0cfdf
khordi <44495894+khordi@users.noreply.github.com>
2019-05-27 23:34:42 +0100

    tutorial basics, and text mode to cozmo display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cozmo
+from PIL import Image, ImageDraw, ImageFont
 
 from sympy.interactive import printing
 printing.init_printing(use_latex=True)
@@ -17,8 +18,46 @@ def cozmo_program(self: cozmo.robot.Robot):
     self.say_text("Hello and welcome to Cozmo Learning", play_excited_animation=True,voice_pitch=-1).wait_for_completed()
 
 def cozmo_button(self: cozmo.robot.Robot):
-    self.say_text("hi").wait
+    self.say_text("hi").wait_for_completed()
 
+def expression_variables(self: cozmo.robot.Robot):
+    #self.say_text("Expressions and variables", play_excited_animation=True,voice_pitch=-1).wait_for_completed()
+    self.say_text("An algebraic expression comprises both numbers and variables together with at least one arithmetic operation.",use_cozmo_voice=True, voice_pitch=-1,duration_scalar=0.7).wait_for_completed()
+
+#------------------------------write text on Cozmos face---------------------------------------------
+
+
+def make_text_image(text_to_draw, x, y, font=None):
+
+    text_image = Image.new('RGBA', cozmo.oled_face.dimensions(), (0, 0, 0, 255))    # make a blank image for the text, initialized to opaque black
+
+    dc = ImageDraw.Draw(text_image)                                         # get a drawing context
+
+    dc.text((x, y), text_to_draw, fill=(255, 255, 255, 255), font=font)    # draw the text
+
+    return text_image
+
+#-------get font --------------
+_math_font = None
+try:
+    _math_font = ImageFont.truetype("arial.ttf", 20)
+except:
+    pass
+#--------------------------------
+def make_equation_image():
+    
+    time_text = "test"
+
+    return make_text_image(time_text, 8, 6, _math_font)
+
+def displaytext(robot: cozmo.robot.Robot):
+
+    while True:
+        math_image = make_equation_image()                                           # Create the updated image with this time
+        oled_face_data = cozmo.oled_face.convert_image_to_screen_data(math_image)
+        robot.display_oled_face_image(oled_face_data, 1000.0)                       # display for 1 second
+
+    cozmo.run_program(displaytext)
 
     
 #------------First Screen-------------------------------------------
@@ -58,6 +97,19 @@ class TrigPythWindow(Screen):
 class GeometryWindow(Screen):
     def on_pre_enter(self):
         Window.size = (608,600)
+
+#----------------------------------------------------------------
+#-----------------------Tutorials--------------------------------
+class AlgebraTutorial(Screen):
+        def on_pre_enter(self):
+            Window.size = (608,600)
+        def checkSection(self):
+            self.ids['expression_button'].background_color = 0,1,0,0.5
+        def expression_variables(self):
+            cozmo.run_program(displaytext)
+            cozmo.run_program(expression_variables)        
+        
+
 
 
 class WindowManager(ScreenManager):
